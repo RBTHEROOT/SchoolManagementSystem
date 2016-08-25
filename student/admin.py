7c7df4b61ab9f9036561d746dd268d102558cc16
Raja <hathtech.com@gmail.com>
2016-08-25 22:02:43 +0530

    Added Total admission and fee management system.

--- student/admin.py
+++ student/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
-from .models import Student, Fees
+from .models import Admission,FeeType,FeePaid,FeeStructure
 # Register your models here.
 
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['admissionNumber', 'name', 'std']
     class Meta:
-        model = Student
+        model = Admission
 
-
-class FeesAdmin(admin.ModelAdmin):
-    list_display = ['admNo', 'previousDues', 'amount', 'amountPaid', 'dues']
+class FeeStructureAdmin(admin.ModelAdmin):
+    list_display = ['std', 'feeType', 'amount']
     class Meta:
-        model = Fees
-admin.site.register(Student, StudentAdmin)
-admin.site.register(Fees, FeesAdmin)
+        model = FeeStructure
+
+class FeePaidAdmin(admin.ModelAdmin):
+    list_display = ['receiptNo', 'admissionNumber', 'amount', 'paidAmount', 'dues', 'date']
+    class Meta:
+        model = FeePaid
+
+
+
+admin.site.register(Admission, StudentAdmin)
+admin.site.register(FeeType)
+admin.site.register(FeePaid, FeePaidAdmin)
+admin.site.register(FeeStructure, FeeStructureAdmin)
--- student/models.py
+++ student/models.py
@@ -1,29 +1,49 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    admissionNumber = models.IntegerField(primary_key=True)
+class Admission(models.Model):
+    admissionNumber = models.AutoField(primary_key=True)
     name = models.CharField(max_length=250, null=False)
     std = models.IntegerField(null=False)
     gender = models.CharField(max_length=10, null=False)
     fatherName = models.CharField(max_length=250, null=False)
     mobileNumber = models.IntegerField(null=False)
     address = models.CharField(max_length=250, null=False)
+    transport = models.BooleanField()
 
     def __str__(self):
         return str(self.admissionNumber)
 
 
-class Fees(models.Model):
-    receiptNo = models.IntegerField()
-    date = models.DateField()
-    admNo = models.ForeignKey(Student, on_delete=None)
-    previousDues = models.IntegerField()
+class FeeType(models.Model):
+    feeType = models.CharField(max_length=50, primary_key=True)
+
+    def __str__(self):
+        return self.feeType
+
+class FeeStructure(models.Model):
+    std = models.IntegerField()
+    feeType = models.ForeignKey(FeeType)
     amount = models.IntegerField()
-    amountPaid = models.IntegerField()
+
+    def __str__(self):
+        return str(self.std)
+
+class FeePaid(models.Model):
+    receiptNo = models.AutoField(primary_key=True)
+    date = models.DateField(auto_now_add=True)
+    admissionNumber = models.ForeignKey(Admission)
+    feeOfMonth = models.CharField(max_length=12)
+    previousDues = models.IntegerField(default=0)
+    amount = models.IntegerField()
+    paidAmount = models.IntegerField()
     dues = models.IntegerField()
 
     def __str__(self):
-        return str(self.admNo)
+        return str(self.admissionNumber)
+
+
+
+
+
