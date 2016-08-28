d941bf70c3658481e612e9a26a3028a9ae44c832
Raja <hathtech.com@gmail.com>
2016-08-28 12:05:02 +0530

    Edited Models and added much more fee model

--- student/admin.py
+++ student/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Admission,FeeType,FeeStructure
+from .models import Admission,FeeType,FeeStructure,FeeReceived,Month,Std
 # Register your models here.
 
 admin.site.register(Admission)
 admin.site.register(FeeType)
 admin.site.register(FeeStructure)
+admin.site.register(FeeReceived)
+admin.site.register(Month)
+admin.site.register(Std)
--- student/models.py
+++ student/models.py
@@ -2,10 +2,39 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Std(models.Model):
+    standard = models.CharField(max_length=3)
+    def __str__(self):
+        return self.standard
+
+class FeeType(models.Model):
+    typeOfFee = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.typeOfFee
+
+
+class Month(models.Model):
+    monthName = models.CharField(max_length=15)
+    def __str__(self):
+        return self.monthName
+
+class FeeStructure(models.Model):
+
+    class Meta:
+        unique_together = (('feeType', 'std'), )
+    feeType = models.ForeignKey(FeeType)
+    std = models.ForeignKey(Std)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return str(self.std)+ ' - ' + self.feeType.typeOfFee
+
 class Admission(models.Model):
     admissionNumber = models.AutoField(primary_key=True)
     studentName = models.CharField(max_length=250, null=False)
-    std = models.IntegerField(null=False)
+    std = models.ForeignKey(Std)
     date = models.DateTimeField(auto_now_add=True, auto_now=False)
     gender = models.CharField(max_length=10, null=False)
     fatherName = models.CharField(max_length=250, null=False)
@@ -15,16 +44,18 @@ class Admission(models.Model):
 
     def __str__(self):
         return str(self.admissionNumber)
-class FeeType(models.Model):
-    typeOfFee = models.CharField(max_length=25)
+
+
+class FeeReceived(models.Model):
+    admNo = models.ForeignKey(Admission, related_name='+')
+    std = models.CharField(max_length=3)
+    date = models.DateField(auto_now_add=True)
+    feeType = models.ManyToManyField(FeeType)
+    feeOfMonth = models.ManyToManyField(Month)
+    previousDues = models.IntegerField()
+    amount = models.IntegerField()
+    paidAmount = models.IntegerField()
+    dues = models.IntegerField()
 
     def __str__(self):
-        return self.typeOfFee
-
-class FeeStructure(models.Model):
-
-    class Meta:
-        unique_together = (('feeType', 'std'), )
-    feeType = models.ForeignKey(FeeType)
-    std = models.CharField(max_length=10)
-    amount = models.IntegerField()
+        return str(self.admNo)
